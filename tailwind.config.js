a3bd6676b8101f7c5515c87e7483309390c20761
Liam <liamsarsfield1499@gmail.com>
2024-04-05 23:40:50 +0100

    Migrating to GKE

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,33 @@
-const colors = require('tailwindcss/colors');
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  mode: 'jit',
-  content: [
-    './public/**/*.html',
-    './src/**/*.{js,jsx,ts,tsx,vue}',
-  ],
+  mode: "jit",
+  content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
   media: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         teal: colors.teal,
         /** variables taken from quasar.variables.scss */
-        primary: '#001427',
-        secondary: '#708D81',
-        accent: '#BF0603',
-        tertiary: '#F4D58D',
-        quaternary: '#8D0801',
-        dark: '#1d1d1d',
+        primary: "#001427",
+        secondary: "#34405d",
+        accent: "#BF0603",
+        tertiary: "#F4D58D",
+        quaternary: "#8D0801",
+        dark: "#1d1d1d",
       },
       transitionProperty: {
-        'bg-width': 'background-color, width',
+        "bg-width": "background-color, width",
       },
     },
     screens: {
-      'sm': '1024px',
-      'md': '1440px',
-      'lg': '1920px',
-      'xl': '2560px',
-    }
+      sm: "1024px",
+      md: "1440px",
+      lg: "1920px",
+      xl: "2560px",
+    },
   },
-  prefix: 'tw-',
+  prefix: "tw-",
   variants: {},
   plugins: [],
 };
